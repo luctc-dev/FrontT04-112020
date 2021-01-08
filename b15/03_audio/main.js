2ede42b43b6c4c0bd91860d20c170ba044e925e2
Tran Cong Luc <luctc.developer1902@gmail.com>
2021-01-08 21:29:58 +0700

    audio

--- b15/03_audio/main.js
+++ b15/03_audio/main.js
@@ -6,6 +6,7 @@ let currentTimeEl = document.querySelector('.current-time');
 let sliderBarEl = document.querySelector('.slider__bar');
 let sliderBarCircleEl = document.querySelector('.slider__bar-circle');
 let durationBarEl = document.querySelector('.duration-bar');
+let customAudioWrapperEl = document.querySelector('.custom-audio');
 
 function handlePlayAudio() {
   audioEl.play();
@@ -67,15 +68,88 @@ function handleClickDurationBar(evt) {
   audioEl.currentTime = percent * audioEl.duration;
 }
 
+function handleDragDurationBar(evt) {
+  // let customAudioRectDom = customAudioWrapperEl.getBoundingClientRect();
+  // let durationBarRectDom = durationBarEl.getBoundingClientRect();
+  // let customAudioWithViewport_x = customAudioRectDom.left;
+  // let durationBarWithViewport_x = durationBarRectDom.left;
+  // let offsetLeftDurationBarWithCustomAudioEl = durationBarWithViewport_x - customAudioWithViewport_x;
+  // let offsetXMouseWithCustomAudioEl = evt.offsetX;
+  // offsetXMouseWithDurationBarEl = offsetXMouseWithCustomAudioEl - offsetLeftDurationBarWithCustomAudioEl;  
+
+  let durationBarRectDom = durationBarEl.getBoundingClientRect();
+  let clientX_Mouse = evt.clientX;
+  let clientX_DurationBar = durationBarRectDom.left;
+  let offsetXMouseWithDurationBarEl = clientX_Mouse - clientX_DurationBar;
+
+  // if (
+  //   offsetXMouseWithDurationBarEl <= 0 || 
+  //   offsetXMouseWithDurationBarEl >= durationBarRectDom.width
+  // ) {
+  //   // console.log('nằm ngoài vùng điều chỉnh thanh thời gian')
+  // } else {
+  //   let percent = offsetXMouseWithDurationBarEl / durationBarRectDom.width;
+  //   audioEl.currentTime = percent * audioEl.duration;
+  // }
+
+  if (
+    offsetXMouseWithDurationBarEl >= 0 && 
+    offsetXMouseWithDurationBarEl <= durationBarRectDom.width
+  ) {
+    let percent = offsetXMouseWithDurationBarEl / durationBarRectDom.width;
+    audioEl.currentTime = percent * audioEl.duration;
+  }
+}
+
 playBtn.addEventListener('click', handlePlayAudio);
 pauseBtn.addEventListener('click', handlePauseAudio);
-durationBarEl.addEventListener('click', handleClickDurationBar);
+// durationBarEl.addEventListener('click', handleClickDurationBar);
 
 audioEl.addEventListener('timeupdate', function(evt) {
   updateTimerUI();
   updateDurationBarUI();
 })
 
-// audioEl.addEventListener('loadedmetadata', function(evt) {
-//   updateDurationUI();
-// })
+
+let isMouseDown = false;
+durationBarEl.addEventListener('mousedown', function(evt){
+  isMouseDown = true;
+})
+
+customAudioWrapperEl.addEventListener('mousemove', function(evt){
+  if (isMouseDown) {
+    handleDragDurationBar(evt)
+  }
+})
+
+customAudioWrapperEl.addEventListener('mouseup', function(evt){
+  isMouseDown = false;
+})
+customAudioWrapperEl.addEventListener('mouseleave', function(evt){
+  isMouseDown = false;
+})
+
+document.addEventListener('keyup', function(evt) {
+  if (evt.code === 'ArrowRight') {
+    let newTime = audioEl.currentTime + 5
+    if (newTime > audioEl.duration) {
+      audioEl.currentTime = audioEl.duration;
+      handlePauseAudio();
+    } else {
+      audioEl.currentTime = newTime;
+    }
+  } else if (evt.code === 'ArrowLeft') {
+    let newTime = audioEl.currentTime - 5
+    if (newTime < 0) {
+      audioEl.currentTime = 0;
+    } else {
+      audioEl.currentTime = newTime;
+    }
+  } else if (evt.code === 'Space') {
+    if (audioEl.paused) {
+      handlePlayAudio();
+    } else {
+      handlePauseAudio();
+    }
+  }
+})
